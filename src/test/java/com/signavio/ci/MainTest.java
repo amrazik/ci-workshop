b14c2278056f44e8a9a3e5b4be12e915f1cff87b
Ondrej Janus <janusonx@gmail.com>
2026-03-17 22:44:00 +0100

    Formatting fix

--- src/test/java/com/signavio/ci/MainTest.java
+++ src/test/java/com/signavio/ci/MainTest.java
@@ -1,13 +1,12 @@
 package com.signavio.ci;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class MainTest {
     @ParameterizedTest(name = "Testing number: {0}")
@@ -25,7 +24,6 @@ class MainTest {
     }
 
     public static Stream<Arguments> numbers() {
-        return IntStream.range(0, 1_000)
-            .mapToObj(Arguments::of);
+        return IntStream.range(0, 1_000).mapToObj(Arguments::of);
     }
 }
